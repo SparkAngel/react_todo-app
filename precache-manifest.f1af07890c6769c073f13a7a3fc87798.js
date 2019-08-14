a972ce1fe300c4a991fe7bd1e4fa49c40a4a5968
SparkAngel <igor.gnatenko555@gmail.com>
2019-08-14 14:39:47 +0300

    Updates

---
+++ precache-manifest.f1af07890c6769c073f13a7a3fc87798.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689a5dc99939f2b67bccc99aff433f56",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "4fd0b7c6d67d9a781272",
+    "url": "/react_todo-app/static/css/main.2ec29585.chunk.css"
+  },
+  {
+    "revision": "99153c32677c116c488b",
+    "url": "/react_todo-app/static/js/2.89964905.chunk.js"
+  },
+  {
+    "revision": "4fd0b7c6d67d9a781272",
+    "url": "/react_todo-app/static/js/main.f3418509.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
